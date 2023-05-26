cfa4635bc5f31bcb759f69d61449b13d56073b12
Nevan4 <Katspru45@gmail.com>
2023-05-26 14:39:10 +0200

    check status_code method of get_response. Add small note.

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,11 +3,13 @@ import requests
 
 endpoint = "https://httpbin.org/anything"
 
-get_response = requests.get(endpoint) # API -> Method
+get_response = requests.get(endpoint, data={"query": "Hello Dog"}) # API -> Method
 
 try:
     data = get_response.json()
     print(data)
 except:
     print("Error from server: " + str(get_response.content))
+
+print(get_response.status_code)
 
