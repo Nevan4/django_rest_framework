5f21eaef104f5ae474ca6e9747100684eaacba82
Nevan4 <Katspru45@gmail.com>
2023-05-26 16:19:54 +0200

    Start first time server Check localhost get response

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,6 +2,7 @@ import requests
 
 
 endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/" #"http://127.0.0.1:8000/"
 
 get_response = requests.get(endpoint, data={"query": "Hello Dog"}) # API -> Method
 
