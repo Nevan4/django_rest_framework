08040ba0c1c429b37d3b4e8f611da356f1bf965f
Nevan4 <Katspru45@gmail.com>
2023-05-31 16:27:58 +0200

    small change in view and in basic.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
     #  request is from -> HttpRequest -> Django
+    print(request.GET)
     body = request.body  # gives string of JSON data
     data = {}
     try:
@@ -12,6 +13,7 @@ def api_home(request, *args, **kwargs):
         pass
     print(data)  # see in console on server side
     # print(data.keys())  # to see only keys from json
+    data['params'] = dict(request.GET)
     data['headers'] = dict(request.headers)
     # data['content_type'] = request.content_type
     return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/" #"http://127.0.0.1:8000/"
 
-get_response = requests.get(endpoint, json={"query": "Hello Dog"}) # API -> Method
+get_response = requests.get(endpoint, params={'abc': 123}, json={"query": "Hello Dog"}) # API -> Method
 
 try:
     data = get_response.json()
