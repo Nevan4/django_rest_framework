a8e8ce939b3daa3394bccec97e4a1a60908ab727
Nevan4 <Katspru45@gmail.com>
2023-06-04 17:23:22 +0200

    Add model_to_dict method in views.py, so we can easier get the proper response with all the values assigned to each key of the model

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,17 +1,35 @@
 import json
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
 
 from products.models import Product
-
 
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+        data = model_to_dict(model_data)
+        # data = model_to_dict(model_data, fields=['id', 'title'])
     return JsonResponse(data)
+
+
+
+
+
+
+
+
+
+
+
+# def api_home(request, *args, **kwargs):
+#     model_data = Product.objects.all().order_by("?").first()
+#     data = {}
+#     if model_data:
+#         data['title'] = model_data.title
+#         data['content'] = model_data.content
+#         data['price'] = model_data.price
+#     return JsonResponse(data)
 
 
     # return JsonResponse({"message": "Hello there, Django API response here!"})
