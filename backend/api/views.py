cda621663d11d7ad66dcafc5b2feaab88e13b84d
Nevan4 <Katspru45@gmail.com>
2023-06-05 19:08:49 +0200

    Add api_view decorator with "GET" request option using rest_framework.decorators api_view. Add rest_framework to settings.py installed apps add note

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,20 @@
 import json
-from django.http import JsonResponse
 from django.forms.models import model_to_dict
+# from django.http import JsonResponse
 
+from rest_framework.response import Response
+
+from rest_framework.decorators import api_view
 from products.models import Product
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
         data = model_to_dict(model_data)
         # data = model_to_dict(model_data, fields=['id', 'title'])
-    return JsonResponse(data)
+    return Response(data)
 
 
 
@@ -22,6 +26,22 @@ def api_home(request, *args, **kwargs):
 
 
 
+
+
+#v2
+
+# def api_home(request, *args, **kwargs):
+#     model_data = Product.objects.all().order_by("?").first()
+#     data = {}
+#     if model_data:
+#         data = model_to_dict(model_data)
+#         # data = model_to_dict(model_data, fields=['id', 'title'])
+#     return JsonResponse(data)
+
+
+
+
+#v1
 # def api_home(request, *args, **kwargs):
 #     model_data = Product.objects.all().order_by("?").first()
 #     data = {}
