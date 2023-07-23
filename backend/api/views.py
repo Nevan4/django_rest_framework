4b305fc230d7ef87c237c4f36cecd2e914205302
Nevan4 <Katspru45@gmail.com>
2023-07-23 16:03:14 +0200

    Add get_my_discount in serializers.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,15 +8,28 @@ from rest_framework.decorators import api_view
 from products.models import Product
 from products.serializers import ProductSerializer
 
-@api_view(["GET"])
+
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
-    instance = Product.objects.all().order_by("?").first()
-    data = {}
-    if instance:
-        # data = model_to_dict(instance)
-        # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
-        data = ProductSerializer(instance).data
-    return Response(data)
+
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        # instance = serializer.save()
+        print(serializer.data)
+        return Response(serializer.data)
+
+
+
+#v4
+# @api_view(["GET"])
+# def api_home(request, *args, **kwargs):
+#     instance = Product.objects.all().order_by("?").first()
+#     data = {}
+#     if instance:
+#         # data = model_to_dict(instance)
+#         # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
+#         data = ProductSerializer(instance).data
+#     return Response(data)
 
 
 
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,14 +4,15 @@ import requests
 endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/" #"http://127.0.0.1:8000/"
 
-get_response = requests.get(endpoint, json={"query": "Hello Dog"}) # HTTP Request
+# get_response = requests.get(endpoint, json={"query": "Hello Dog"}) # HTTP Request
+get_response = requests.post(endpoint, json={"content": None}) # HTTP Request
 
-try:
-    data = get_response.json()
-    print(data)
-except:
-    print("Error from server: " + str(get_response.content))
+# try:
+#     data = get_response.json()
+#     print(data)
+# except:
+#     print("Error from server: " + str(get_response.content))
 
 # print("status code: " + str(get_response.status_code))
 
-# print(get_response.json())
+print(get_response.json())
