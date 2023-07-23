a05396d20c64657bc34d4342a7798e2195da4358
Nevan4 <Katspru45@gmail.com>
2023-07-23 18:27:10 +0200

    Handle POST methods Biggest changes in views.py and serializers.py Add request.post method in basic.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -46,7 +46,7 @@ def api_home(request, *args, **kwargs):
 # def api_home(request, *args, **kwargs):
 #     model_data = Product.objects.all().order_by("?").first()
 #     data = {}
-#     if model_data:
+ #     if model_data:
 #         # data = model_to_dict(model_data)
 #         data = model_to_dict(model_data, fields=['id', 'title', 'price', 'sale_price'])
 #     return Response(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,7 +5,8 @@ endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/" #"http://127.0.0.1:8000/"
 
 # get_response = requests.get(endpoint, json={"query": "Hello Dog"}) # HTTP Request
-get_response = requests.post(endpoint, json={"content": None}) # HTTP Request
+get_response = requests.post(endpoint, json={"title": "SOme title", "content": None,
+                                             "price": 100}) # HTTP Request
 
 # try:
 #     data = get_response.json()
